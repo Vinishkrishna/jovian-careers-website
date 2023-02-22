9dcf14c55563b7ee69b714de2cc03e1c9bf84127
Vinish Krishna <96462939+Vinishkrishna@users.noreply.github.com>
2023-02-22 14:03:17 +0000

    basic layout created

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-#Flask is a clask
+#Flask is a class
 from flask import Flask, render_template
 #render template to include the html file
 #Flask object created
@@ -15,3 +15,5 @@ def hello_world():
 if __name__ == '__main__':
   #to run the program on local device
   app.run(host='0.0.0.0', debug=True)
+#before creating a website one should first make a outline of website either through tools like figma/in paper.
+#anything inside static folder is available to home.html like files.
