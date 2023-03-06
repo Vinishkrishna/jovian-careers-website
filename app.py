c8c8ba1217bd07f46ee3bbcea519d86cea4ff786
Vinish Krishna <96462939+Vinishkrishna@users.noreply.github.com>
2023-03-06 17:20:58 +0000

    added dynamic data  and API route

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 #Flask is a class
-from flask import Flask, render_template
+from flask import Flask, render_template,jsonify
+#jsonify converts anything into json object
 #render template to include the html file
 #Flask object created
 app = Flask(__name__)
@@ -29,14 +30,17 @@ JOBS=[
     'salary':'$120,000'
   }
 ]
-#route stands for everything after domain name
+#/route stands for everything after domain name
 #@ is used to provide some advanced functionality to the library
 @app.route("/")
 #jobs is like an argument provided to home.html
 def hello_jovian():
   return render_template("home.html",jobs=JOBS,company_name='Jovian')
  
-
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
+  
 if __name__ == '__main__':
   #to run the program on local device
   app.run(host='0.0.0.0', debug=True)
