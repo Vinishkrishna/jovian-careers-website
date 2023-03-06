aa2eb647b02f9cddf7b98561b7ec59cf4deb2582
Vinish Krishna <96462939+Vinishkrishna@users.noreply.github.com>
2023-03-06 13:54:20 +0000

    start of adding dynamic content

--- app.py
+++ app.py
@@ -4,13 +4,39 @@ from flask import Flask, render_template
 #Flask object created
 app = Flask(__name__)
 
-
+JOBS=[
+  {
+    'id':1,
+    'title':'Data Analyst',
+    'location':'Bengaluru,India',
+    'salary':'Rs.10,00,000'
+  },
+  {
+    'id':2,
+    'title':'Data Scientist',
+    'location':'Delhi,India',
+    'salary':'Rs.15,00,000'
+  },
+  {
+    'id':3,
+    'title':'Frontend Engineer',
+    'location':'Remote',
+    'salary':'Rs.12,00,000'
+  },
+  {
+    'id':4,
+    'title':'Backend Engineer',
+    'location':'San Francisco,USA',
+    'salary':'$120,000'
+  }
+]
 #route stands for everything after domain name
 #@ is used to provide some advanced functionality to the library
 @app.route("/")
-def hello_world():
-  return render_template("home.html")
-
+#jobs is like an argument provided to home.html
+def hello_jovian():
+  return render_template("home.html",jobs=JOBS,company_name='Jovian')
+ 
 
 if __name__ == '__main__':
   #to run the program on local device
